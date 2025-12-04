f3aac541b4a5b67dac92b66b6d2ba7a1df74c9f6
Kalaiselvan Arumugam <a.kalaiselvan@live.com>
2025-12-05 03:33:40 +0530

    chore: update gitignore and remove tracked build artifacts

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_mybackup_smbsync_MyBackupApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.mybackup.smbsync.MyBackupApplication"
-)
-public final class _com_mybackup_smbsync_MyBackupApplication {
-}
