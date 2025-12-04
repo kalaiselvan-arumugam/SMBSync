e9227552341ac81232012088ced4b4179f40fb76
Kalaiselvan Arumugam <a.kalaiselvan@live.com>
2025-12-05 02:50:17 +0530

    chore: update libraries for stability + security

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,7 @@ dependencies {
     // Core Android
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.activity:activity-compose:1.8.2")
 
     // Compose BOM
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
